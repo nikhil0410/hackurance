f154c4cfbde888cbf19d0e8fc8424f4bb1c83ecf
Nikhil  Gupta <nikhilgupta@Nikhils-MacBook-Air.local>
2020-05-16 22:59:23 +0530

    added login and registration page, and removed some redundant features

--- quiz/forms.py
+++ quiz/forms.py
@@ -11,3 +11,11 @@ class QuestionForm(forms.Form):
         self.fields["answers"] = forms.ChoiceField(choices=choice_list, widget=RadioSelect)
 
 
+class RegisterForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+    password_repeat = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Repeat Password'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'First Name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Last Name'}))
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Phone Number'}), required=False)
